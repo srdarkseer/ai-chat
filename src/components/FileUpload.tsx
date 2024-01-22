115c952362231e599a59f61f158c67bf7f423cc7
Sushant R. Dangal <srdarkseer@gmail.com>
2024-01-22 22:18:06 +0545

    updated upload section

--- src/components/FileUpload.tsx
+++ src/components/FileUpload.tsx
@@ -1,9 +1,10 @@
 import React, { useState } from "react";
 import { useDropzone } from "react-dropzone";
-import { Inbox, XCircle } from "lucide-react"; // Import XCircle from lucide-react
+import { Plus, XCircle } from "lucide-react";
+import { FaFilePdf, FaFileWord } from "react-icons/fa";
 
 const FileUpload = () => {
-  const [uploadedFile, setUploadedFile] = useState<File | null>(null);
+  const [uploadedFiles, setUploadedFiles] = useState<File[]>([]);
   const { getRootProps, getInputProps, open } = useDropzone({
     accept: {
       "application/pdf": [".pdf"],
@@ -11,50 +12,71 @@ const FileUpload = () => {
       "application/vnd.openxmlformats-officedocument.wordprocessingml.document":
         [".docx"],
     },
-    maxFiles: 1,
     noClick: true,
     noKeyboard: true,
     onDrop: (acceptedFiles) => {
-      const file = acceptedFiles[0];
-      if (file.size > 10 * 1024 * 1024) {
-        alert("File is too big! Please upload a file smaller than 10mb.");
-        return;
-      }
-      setUploadedFile(file);
+      const validFiles = acceptedFiles.filter(
+        (file) => file.size <= 10 * 1024 * 1024
+      );
+      setUploadedFiles([...uploadedFiles, ...validFiles]);
     },
   });
 
-  const handleRemoveFile = () => {
-    setUploadedFile(null); // Reset the uploadedFile state
+  const handleRemoveFile = (index: number) => {
+    const updatedFiles = [...uploadedFiles];
+    updatedFiles.splice(index, 1);
+    setUploadedFiles(updatedFiles);
+  };
+
+  const getFileIcon = (fileName: string) => {
+    if (fileName.endsWith(".pdf")) {
+      return <FaFilePdf className="w-6 h-6 text-red-600" />;
+    } else if (fileName.endsWith(".doc") || fileName.endsWith(".docx")) {
+      return <FaFileWord className="w-6 h-6 text-blue-600" />;
+    } else {
+      return null;
+    }
   };
 
   return (
-    <div className="p-2 bg-white rounded-xl">
-      {uploadedFile ? (
-        <div className="w-full flex justify-between items-center p-4 bg-gray-50 rounded-xl">
-          <span className="text-sm text-slate-500">{uploadedFile.name}</span>
-          <button
-            onClick={handleRemoveFile}
-            className="text-red-500 hover:text-red-600"
+    <div className="grid grid-cols-10 gap-5">
+      <div className="col-span-2">
+        <div className="p-2 bg-white rounded-xl w-44 h-40">
+          <div
+            {...getRootProps({
+              className:
+                "w-full h-36 border-dashed border-2 rounded-xl cursor-pointer bg-gray-50 py-8 flex justify-center items-center flex-col",
+              onClick: (event) => open(),
+            })}
           >
-            <XCircle className="w-6 h-6" />
-          </button>
+            <input {...getInputProps()} />
+            <>
+              <Plus className="w-10 h-10 text-slate" />
+              <p className="mt-2 text-sm text-slate-400">Drop files here</p>
+            </>
+          </div>
         </div>
-      ) : (
-        <div
-          {...getRootProps({
-            className:
-              "w-full border-dashed border-2 rounded-xl cursor-pointer bg-gray-50 py-8 flex justify-center items-center flex-col",
-            onClick: (event) => open(),
-          })}
-        >
-          <input {...getInputProps()} />
-          <>
-            <Inbox className="w-10 h-10 text-slate" />
-            <p className="mt-2 text-sm text-slate-400">Drop PDF Here</p>
-          </>
+      </div>
+
+      {/* Display uploaded files */}
+      {uploadedFiles.map((file, index) => (
+        <div key={index} className="col-span-2 relative">
+          <div className="p-2 bg-white rounded-xl w-44 h-40 flex justify-center items-center">
+            <div className="flex flex-col gap-3 items-center p-4 bg-gray-50 rounded-xl w-full">
+              {getFileIcon(file.name)}
+              <span className="text-sm text-slate-500 truncate">
+                {file.name}
+              </span>
+              <button
+                onClick={() => handleRemoveFile(index)}
+                className="absolute top-0 right-0 p-1 text-red-500 hover:text-red-600"
+              >
+                <XCircle className="w-6 h-6" />
+              </button>
+            </div>
+          </div>
         </div>
-      )}
+      ))}
     </div>
   );
 };
--- src/pages/Admin/Upload.tsx
+++ src/pages/Admin/Upload.tsx
@@ -3,9 +3,9 @@ import FileUpload from "../../components/FileUpload";
 
 const Upload = () => {
   return (
-    <div className="flex h-screen items-center">
+    <div className="flex h-screen">
       <div
-        className={`px-10 flex-1 flex flex-col bg-gray-100 transition-all duration-300`}
+        className={`px-40 py-10 flex-1 flex flex-col bg-gray-100 transition-all duration-300`}
       >
         <FileUpload />
       </div>
