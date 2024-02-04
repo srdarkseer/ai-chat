b112db7a6ac5f6ded330f706856cad55e262b8aa
Sushant R. Dangal <srdarkseer@gmail.com>
2024-02-04 22:09:56 +0545

    Revert "added loading"
    This reverts commit 3b61f98aba7b001e7152d8a5ec459e54e35651bc.

--- src/pages/User/ChatInterface.tsx
+++ src/pages/User/ChatInterface.tsx
@@ -6,8 +6,8 @@ import Loader from "../../assets/images/loading.svg";
 
 const ChatInterface = () => {
   const [messages, setMessages] = useState([
-    { id: 1, text: "Hi", sender: "user", loading: false },
-    { id: 2, text: "Hello! How can I assist you today?", sender: "ai", loading: false },
+    { id: 1, text: "Hi", sender: "user" },
+    { id: 2, text: "Hello! How can I assist you today?", sender: "ai" },
   ]);
   const [input, setInput] = useState("");
   const [loading, setLoading] = useState(false);
@@ -24,24 +24,13 @@ const ChatInterface = () => {
     if (input.trim()) {
       setLoading(true);
 
-      const count = messages.length;
-
       const newMessage = {
-        id: count + 1,
+        id: messages.length + 1,
         text: input.replace(/(?:\r\n|\r|\n)/g, "<br>"),
         sender: "user",
       };
 
-      const aiMessage = {
-        id: count + 2, 
-        text: "Hello! How can I assist you today?", 
-        sender: "ai", 
-        loading: true,
-      }
-
-      const messageArr = [newMessage, aiMessage];
-
-      setMessages([...messages, ...messageArr]);
+      setMessages([...messages, newMessage]);
       setInput("");
 
       if (textAreaRef.current) {
@@ -92,53 +81,35 @@ const ChatInterface = () => {
         ref={messagesContainerRef}
         className="flex-1 overflow-auto flex flex-col-reverse"
       >
-        {[...messages].reverse().map((message) => {
-          if (message.sender === "user") {
-            return (
-              <div key={message.id}>
-                <div
-                  className={`flex items-end space-x-2 py-4 px-40 bg-gray-200`}
-                >
-                  <img
-                    src={openAIImage}
-                    alt={`${message.sender} Avatar`}
-                    className="w-10 h-10 object-cover rounded-full"
-                  />
-                  <div className="flex flex-col flex-1">
-                  {loading && message.id === messages.length && (
-                    <div className="flex justify-center py-2">
-                      <img src={Loader} className="w-20 h-20" alt="Loading..." />
-                    </div>
-                  )}
-                    <div className="p-2 rounded text-black whitespace-pre-wrap">
-                      {formatMessageText(message.text)}
-                    </div>
-                  </div>
+        {[...messages].reverse().map((message) => (
+          <div key={message.id}>
+            <div
+              className={`flex items-end space-x-2 py-4 px-40 ${
+                message.sender === "ai" ? "bg-gray-200" : "bg-gray-100"
+              }`}
+            >
+              <img
+                src={
+                  message.sender === "ai"
+                    ? openAIImage
+                    : "https://github.com/shadcn.png"
+                }
+                alt={`${message.sender} Avatar`}
+                className="w-10 h-10 object-cover rounded-full"
+              />
+              <div className="flex flex-col flex-1">
+                <div className="p-2 rounded text-black whitespace-pre-wrap">
+                  {formatMessageText(message.text)}
                 </div>
               </div>
-            );
-          } else {
-            return (
-              <div key={message.id}>
-                <div
-                  className={`flex items-end space-x-2 py-4 px-40 bg-gray-100`}
-                >
-                  <img
-                    src={"https://github.com/shadcn.png"}
-                    alt={`${message.sender} Avatar`}
-                    className="w-10 h-10 object-cover rounded-full"
-                  />
-                  <div className="flex flex-col flex-1">
-                    {loading && message.id === messages.length && (<span className="loader"></span>)}
-                    <div className="p-2 rounded text-black whitespace-pre-wrap">
-                      {formatMessageText(message.text)}
-                    </div>
-                  </div>
-                </div>
+            </div>
+            {loading && message.id === messages.length && (
+              <div className="flex justify-center py-2">
+                <img src={Loader} className="w-20 h-20" alt="Loading..." />
               </div>
-            );
-          }
-        })}
+            )}
+          </div>
+        ))}
       </div>
       <div className="py-4 px-40 border-t border-gray-200">
         <form onSubmit={sendMessage} className="flex items-center gap-2">
